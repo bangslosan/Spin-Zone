61fdd0b88c49f442f684ef49f0acd2138d11aa92
Nicholas714 <grananicholas@gmail.com>
2017-06-16 12:40:22 -0400

    Initial Commit

---
+++ Spin-Zone macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Spin-Zone macOS
+//
+//  Created by Nicholas Grana on 6/16/17.
+//  Copyright © 2017 Nicholas Grana. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
